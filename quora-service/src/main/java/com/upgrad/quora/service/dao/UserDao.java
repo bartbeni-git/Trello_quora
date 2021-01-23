03f625c7dc2da56a9c988f6f674624ee28fe6d48
susri1125 <susri1125@gmail.com>
2021-01-23 11:00:40 +0530

    Adding SignIn Feature

--- quora-service/src/main/java/com/upgrad/quora/service/dao/UserDao.java
+++ quora-service/src/main/java/com/upgrad/quora/service/dao/UserDao.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Repository;
 import com.upgrad.quora.service.entity.UserEntity;
 
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
 
 @Repository
@@ -19,5 +20,24 @@ public class UserDao {
 
   public UserEntity getUser(final String userUuid) {
     return entityManager.createNamedQuery("userByUuid", UserEntity.class).setParameter("uuid", userUuid).getSingleResult();
+  }
+
+  /**
+   * This methods gets the user details based on the username passed.
+   *
+   * @param userName username of the user whose information is to be fetched.
+   * @return null if the user with given username doesn't exist in DB.
+   */
+  public UserEntity getUserByUserName(final String userName) {
+    try {
+      return entityManager.createNamedQuery("userByUserName", UserEntity.class).setParameter("userName", userName).getSingleResult();
+    } catch (NoResultException nre) {
+      return null;
+    }
+  }
+
+
+  public void updateUserEntity(final UserEntity updatedUserEntity) {
+    entityManager.merge(updatedUserEntity);
   }
 }

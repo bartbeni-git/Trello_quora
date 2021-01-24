b3a8b446d8703cdde4750aaf292ab75dce52ef85
Anuj Upadhyay <anuj4u.18@gmail.com>
2021-01-24 10:02:58 -0800

    Add question edit functionality
    Unit tests passing

--- quora-service/src/main/java/com/upgrad/quora/service/business/QuestionBusinessService.java
+++ quora-service/src/main/java/com/upgrad/quora/service/business/QuestionBusinessService.java
@@ -7,9 +7,11 @@ import com.upgrad.quora.service.entity.UserAuthEntity;
 import com.upgrad.quora.service.exception.AuthorizationFailedException;
 import com.upgrad.quora.service.exception.InvalidQuestionException;
 import com.upgrad.quora.service.exception.UserNotFoundException;
+import java.time.ZonedDateTime;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
@@ -20,6 +22,7 @@ public class QuestionBusinessService {
   @Autowired
   private UserAuthDao userAuthDao;
 
+  @Transactional(propagation = Propagation.REQUIRED)
   public QuestionEntity createQuestion(final String accessToken, final QuestionEntity questionEntity)
       throws AuthorizationFailedException {
     if(accessToken == null) {
@@ -70,7 +73,7 @@ public class QuestionBusinessService {
     return questionsByUuid;
   }
 
-  @Transactional
+  @Transactional(propagation = Propagation.REQUIRED)
   public QuestionEntity deleteQuestion(final String accessToken, final String questionUuid)
       throws AuthorizationFailedException, InvalidQuestionException {
     if(accessToken == null) {
@@ -94,6 +97,34 @@ public class QuestionBusinessService {
       throw new AuthorizationFailedException("ATHR-003", "Only the question owner or admin can delete the question");
     }
     questionDao.deleteQuestion(questionUuid);
+    return questionEntity;
+  }
+
+  @Transactional(propagation = Propagation.REQUIRED)
+  public QuestionEntity updateQuestionEntity(final String accessToken, final String questionUuid, final String newContent)
+      throws AuthorizationFailedException, InvalidQuestionException {
+    if(accessToken == null) {
+      throw new AuthorizationFailedException("ATHR-001", "User has not signed in");
+    }
+    UserAuthEntity userAuthTokenEntity = userAuthDao.getUserAuthByToken(accessToken);
+    if(userAuthTokenEntity ==null) {
+      throw new AuthorizationFailedException("ATHR-001", "User has not signed in");
+    }
+    if(userAuthTokenEntity.getLogoutAt() != null) {
+      throw new AuthorizationFailedException("ATHR-002", "User is signed out.Sign in first to edit the question");
+    }
+
+    QuestionEntity questionEntity = questionDao.getQuestionById(questionUuid);
+
+    if(questionEntity == null) {
+      throw new InvalidQuestionException("QUES-001", "Entered question uuid does not exist");
+    }
+
+    if(userAuthTokenEntity.getUserEntity().getId() != questionEntity.getUserEntity().getId()) {
+      throw new AuthorizationFailedException("ATHR-003", "Only the question owner can edit the question");
+    }
+    questionEntity.setContent(newContent);
+    questionDao.updateQuestion(questionEntity);
     return questionEntity;
   }
 }
